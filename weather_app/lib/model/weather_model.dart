9e982abe549bbc4985bea33debba795b6b105d28
Isagani Lapira <lapiraisagani@gmail.com>
2024-02-26 15:09:07 +0800

    Add: weather model

---
+++ weather_app/lib/model/weather_model.dart
@@ -0,0 +1,11 @@
+class WeatherModel {
+  final double temp;
+  final String weatherDesc;
+  final double windSpeed;
+
+  WeatherModel({
+    required this.temp,
+    required this.windSpeed,
+    required this.weatherDesc,
+  });
+}
